04cdfbc02f9c67dae6ab2af2e6fcb6a3cc4a8fa7
Nellyx <neilsonmike14@gmail.com>
2022-05-29 16:11:36 -0700

    my second commit

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,36 +3,27 @@ import React, { useEffect, useState } from "react";
 const useFetch = (url) => {
   const [data, setdata] = useState(null);
   const [ispending, setispending] = useState(false);
-  const [error, seterror] = useState(null)
+  const [error, seterror] = useState(null);
 
   useEffect(() => {
     const fetchdata = async () => {
+      try {
+        setispending(true);
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
 
+        const json = await response.json();
+        setispending(false);
+        setdata(json);
+        seterror(null);
+      } catch (error) {
+        seterror("There is an error");
+        setispending(false);
 
-try {
-  setispending(true);
-  const response = await fetch(url);
-
-
-
-
-
-  
-  const json = await response.json();
-  setispending(false);
-  setdata(json);
-  
-} catch (error) {
-  seterror("There is an error")
-  setispending(false)
-  
-  console.log(error.message)
-}
-
-
-
-
-
+        console.log(error.message);
+      }
     };
 
     fetchdata();
