8c468a2d69be7fc0f51426a86901c7f749bd40bf
Nellyx <neilsonmike14@gmail.com>
2022-06-01 08:32:17 -0700

    7th commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,12 +2,14 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 function Contact() {
-  // const queryString = useLocation().search;
-  // console.log(queryString);
+  const queryString = useLocation().search;
+  console.log(queryString);
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name");
 
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>hey {name} Contact us Here</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, placeat!
         Alias facilis, eos reiciendis blanditiis et harum quidem, recusandae
